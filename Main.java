a655ef4e84105c85697821a34a013a4468d7e1ff
Diederick Prosman <51356@hoornbeeck.nl>
2025-02-07 17:34:25 +0100

    made it work

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.ActionListener;
 
 public class Main {
 
@@ -26,59 +27,6 @@ static JFrame frame = new JFrame("Calculator");
     
     JLabel label = new JLabel("Testing");
     label.setBounds(100, 0, 150, 20);
-
-    /*JButton een = new JButton("1");
-    een.setBounds(250, 100, 100, 30);
-
-    JButton twee = new JButton("2");
-    twee.setBounds(350, 100, 100, 30);
-
-    JButton drie = new JButton("3");
-    drie.setBounds(450, 100, 100, 30);
-
-    JButton vier = new JButton("4");
-    vier.setBounds(250, 130, 100, 30);
-
-    JButton vijf = new JButton("5");
-    vijf.setBounds(350, 130, 100, 30);
-
-    JButton zes = new JButton("6");
-    zes.setBounds(450, 130, 100, 30);
-
-    JButton zeven = new JButton("7");
-    zeven.setBounds(250, 160, 100, 30);
-
-    JButton acht = new JButton("8");
-    acht.setBounds(350, 160, 100, 30);
-
-    JButton negen = new JButton("9");
-    negen.setBounds(450, 160, 100, 30);
-
-    JButton nul = new JButton("0");
-    nul.setBounds(250, 190, 100, 30);
-
-    een.addActionListener(new ActionListener() {
-        public void actionPerformed(ActionEvent e) {
-            a = a + "1";
-            label.setText(a);
-        }
-    });
-
-    frame.add(een);
-    frame.add(twee);
-    frame.add(drie);
-
-    frame.add(vier);
-    frame.add(vijf);
-    frame.add(zes);
-
-    frame.add(zeven);
-    frame.add(acht);
-    frame.add(negen);
-
-    frame.add(nul);   
-
-    frame.add(label);*/
 
     frame.setVisible(true);
     }
